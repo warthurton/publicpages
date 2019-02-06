84ad13588a2006e14fce272f43e1507f77792987
Wayne Arthurton <warthurton@arthurton.com>
2019-02-06 17:32:39 -0500

    Update createReadme.py

--- createReadme.py
+++ createReadme.py
@@ -29,6 +29,7 @@ Inspired by [thoughtbot/til](https://github.com/thoughtbot/til).
 * [Today I Learned by Hashrocket](https://til.hashrocket.com)
 * [jwworth/til](https://github.com/jwworth/til)
 * [thoughtbot/til](https://github.com/thoughtbot/til)
+* [secretgeek/til](https://github.com/secretGeek/til)
 
 ## License
 
